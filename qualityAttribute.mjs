078687ec99d3c4c89b07333d7772524a67af4358
ABISHEK A T <43531987+abishekat@users.noreply.github.com>
2022-12-06 13:43:00 -0500

    Add files via upload

---
+++ qualityAttribute.mjs
@@ -0,0 +1,26 @@
+import fetch from "node-fetch";
+
+const body = { username: "abishek", password: "1234" };
+
+const response = await fetch(
+  "https://pf4yj9fivh.execute-api.us-east-1.amazonaws.com/prod-env/login",
+  {
+    method: "post",
+    body: JSON.stringify(body),
+    headers: { "Content-Type": "application/json" },
+  }
+);
+var jsonArray = [];
+const data = await response.json();
+const token = data.token;
+for (var i = 0; i < 1; i++) {
+  var dateTimeStamp = Date.now();
+  const plot = {
+    time: dateTimeStamp,
+    data: token,
+  };
+
+  jsonArray.push(plot);
+}
+
+console.log(jsonArray);
