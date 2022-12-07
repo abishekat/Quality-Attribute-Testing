df529100b79180b729203cf27a9a6b544040defd
ABISHEK A T <43531987+abishekat@users.noreply.github.com>
2022-12-06 21:16:43 -0500

    final commit

--- qualityAttribute.mjs
+++ qualityAttribute.mjs
@@ -1,19 +1,19 @@
 import fetch from "node-fetch";
 
 const body = { username: "abishek", password: "1234" };
-
-const response = await fetch(
-  "https://pf4yj9fivh.execute-api.us-east-1.amazonaws.com/prod-env/login",
-  {
-    method: "post",
-    body: JSON.stringify(body),
-    headers: { "Content-Type": "application/json" },
-  }
-);
 var jsonArray = [];
-const data = await response.json();
-const token = data.token;
+
 for (var i = 0; i < 100; i++) {
+  const response = await fetch(
+    "https://pf4yj9fivh.execute-api.us-east-1.amazonaws.com/prod-env/login",
+    {
+      method: "post",
+      body: JSON.stringify(body),
+      headers: { "Content-Type": "application/json" },
+    }
+  );
+  const data = await response.json();
+  const token = data.token;
   var dateTimeStamp = Date.now();
   const plot = {
     time: dateTimeStamp,
